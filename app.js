b68127356f525819f7ebbcb71195d16f577071c4
SaintOfMirren <drew.miller2015@gmail.com>
2023-06-11 10:57:59 -0700

    updated routes and schema

--- app.js
+++ app.js
@@ -3,12 +3,16 @@ const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const sequelize = require('./sequelize');
 const db = require('./models');
+const exphbs = require('express-handlebars');
 const routes = require('./controllers');
+const hbs = exphbs.create({});
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const hbs = exphbs.create({ helpers });
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 // Set up session middleware
 app.use(
--- controllers/api/blogRoutes.js
+++ controllers/api/blogRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { User, Article } = require('../models');
+const { User, Article } = require('../../models');
 
 // GET all articles
 router.get('/', async (req, res) => {
